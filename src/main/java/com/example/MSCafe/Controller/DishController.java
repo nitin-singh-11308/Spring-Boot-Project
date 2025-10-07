810da107c9bf0f255107c7af2b7b4316a9f31f68
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-08 05:08:36 +0530

    change api path dish to dishes

--- src/main/java/com/example/MSCafe/Controller/DishController.java
+++ src/main/java/com/example/MSCafe/Controller/DishController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/dish")
+@RequestMapping("/api/dishes")
 public class DishController {
     @Autowired
     private DishService dishService;
