435e3726f8b7605f30501c2ed34eaad2df69ed95
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-08 05:19:54 +0530

    Add Global exception Handler and Handle ChefNotFountException

--- src/main/java/com/example/MSCafe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/MSCafe/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.example.MSCafe.exception;
 
+import com.example.MSCafe.dto.response.GenericResponseDto;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ChefNotFoundException.class)
+    public ResponseEntity<GenericResponseDto> handleChefNotFoundException(ChefNotFoundException e) {
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage(e.getMessage());
+        genericResponseDto.setDetail(null);
+
+        return new ResponseEntity<> (genericResponseDto,HttpStatusCode.valueOf(404));
+    }
 }
