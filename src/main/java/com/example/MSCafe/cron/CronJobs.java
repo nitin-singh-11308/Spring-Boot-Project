3c5bce642c7727ecb462680ea1fbfae3dc34e9af
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-08 05:13:43 +0530

    comment out cronjob for a while

--- src/main/java/com/example/MSCafe/cron/CronJobs.java
+++ src/main/java/com/example/MSCafe/cron/CronJobs.java
@@ -1,22 +1,22 @@
-package com.example.MSCafe.cron;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CronJobs {
-    int count = 0;
-
-    @Scheduled(cron = "* * * * * ?")
-    private void randomJob() {
-        System.out.println("Hello" + ++count);
-        if (count == 60) {
-            count = 0;
-        }
-    }
-
-    @Scheduled(cron = "${my.cron.expression}")
-    private void random() {
-        System.out.println("Hello From CronJob");
-    }
-}
+//package com.example.MSCafe.cron;
+//
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CronJobs {
+//    int count = 0;
+//
+//    @Scheduled(cron = "* * * * * ?")
+//    private void randomJob() {
+//        System.out.println("Hello" + ++count);
+//        if (count == 60) {
+//            count = 0;
+//        }
+//    }
+//
+//    @Scheduled(cron = "${my.cron.expression}")
+//    private void random() {
+//        System.out.println("Hello From CronJob");
+//    }
+//}
