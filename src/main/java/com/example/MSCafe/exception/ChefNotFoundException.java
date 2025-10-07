47d4992090e2bb74531bedbac4b664aa26e3c8ad
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-08 05:17:16 +0530

    Create a Exception class for control the exception in ChefServiceImpl

--- src/main/java/com/example/MSCafe/exception/ChefNotFoundException.java
+++ src/main/java/com/example/MSCafe/exception/ChefNotFoundException.java
@@ -1,4 +1,11 @@
 package com.example.MSCafe.exception;
 
-public class ChefNotFoundException {
+public class ChefNotFoundException extends RuntimeException {
+    public ChefNotFoundException() {
+        super("Chef Doesn't Found");
+    }
+
+    public ChefNotFoundException(String m) {
+        super(m);
+    }
 }
