6ff90a1b8c6834d1dd97644a0d12ff4002801f8e
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-16 11:00:05 +0530

    Add Server Controller Which will return many Attributes

--- src/main/java/com/example/MSCafe/Controller/ServerController.java
+++ src/main/java/com/example/MSCafe/Controller/ServerController.java
@@ -1,17 +1,33 @@
 package com.example.MSCafe.Controller;
 
 import com.example.MSCafe.dto.response.ServerResponseDto;
+import com.example.MSCafe.dto.response.ServerStatusResponseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class ServerController {
+
+    private final Instant serverStartTime;
+    private final String applicationName;
+
+    public ServerController(@Value("${spring.application.name}") String applicationName) {
+        this.serverStartTime = Instant.now();
+        this.applicationName = applicationName;
+    }
+
     @GetMapping
-    public ResponseEntity<ServerResponseDto> serverStatus() {
-        return new ResponseEntity<>(new ServerResponseDto("Server is Live"), HttpStatusCode.valueOf(200));
+    public ResponseEntity<ServerStatusResponseDto> checkServerStatus() {
+        ServerStatusResponseDto serverStatusResponseDto = new ServerStatusResponseDto(serverStartTime, applicationName);
+
+        return new ResponseEntity<>(serverStatusResponseDto, HttpStatusCode.valueOf(200));
     }
 }
