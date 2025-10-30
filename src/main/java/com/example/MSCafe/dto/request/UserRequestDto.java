73113ad09be30e84ae88ca184d76e06542e00524
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-30 17:17:11 +0530

    add user request dto

--- src/main/java/com/example/MSCafe/dto/request/UserRequestDto.java
+++ src/main/java/com/example/MSCafe/dto/request/UserRequestDto.java
@@ -1,4 +1,11 @@
 package com.example.MSCafe.dto.request;
 
+import com.example.MSCafe.enums.UserRole;
+import lombok.Data;
+
+@Data
 public class UserRequestDto {
+    private UserRole role;
+    private String username;
+    private String password;
 }
