2237ed29bec0732d257b81d8a2cadfaa1525834f
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-30 17:03:29 +0530

    add User Controller

--- src/main/java/com/example/MSCafe/Controller/UserController.java
+++ src/main/java/com/example/MSCafe/Controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.MSCafe.Controller;
 
+import com.example.MSCafe.Service.UserService;
+import com.example.MSCafe.dto.request.UserRequestDto;
+import com.example.MSCafe.dto.response.UserResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> signup(@RequestBody UserRequestDto userRequestDto) {
+       return new ResponseEntity<>(userService.registerUser(userRequestDto), HttpStatus.CREATED);
+    }
 }
