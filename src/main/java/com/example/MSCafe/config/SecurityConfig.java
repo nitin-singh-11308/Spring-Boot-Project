64421567b33ccf94682300565807a9cf379fb25e
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-30 17:15:35 +0530

    add Security Configuration and permission to User side API

--- src/main/java/com/example/MSCafe/config/SecurityConfig.java
+++ src/main/java/com/example/MSCafe/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.example.MSCafe.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                // Disable CSRF to call All HTTP Methods (POST, PUT, PATCH, DELETE)
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/dishes", "/api/dishes/**").permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/api/dishes", "/api/dishes/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+                                .requestMatchers("/api", "/api/**").authenticated()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return httpSecurity.build();
+    }
 }
