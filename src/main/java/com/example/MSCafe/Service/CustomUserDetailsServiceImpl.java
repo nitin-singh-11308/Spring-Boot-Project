28be36e5694968e0b6384dcc347326378448baa9
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-30 17:08:25 +0530

    add user Details service Impl and handle user not Found Exception

--- src/main/java/com/example/MSCafe/Service/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/example/MSCafe/Service/CustomUserDetailsServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.MSCafe.Service;
 
-public class CustomUserDetailsServiceImpl {
+import com.example.MSCafe.Repository.UserRepository;
+import com.example.MSCafe.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()-> new UserNotFoundException("User name: "+username+" doesn't exist"));
+    }
 }
