157d55e6f4b77182a7526984074031ee24fbb535
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-10-30 17:18:02 +0530

    Add user Response Dto

--- src/main/java/com/example/MSCafe/dto/response/UserResponseDto.java
+++ src/main/java/com/example/MSCafe/dto/response/UserResponseDto.java
@@ -1,4 +1,11 @@
 package com.example.MSCafe.dto.response;
 
+import com.example.MSCafe.enums.UserRole;
+import lombok.Data;
+
+@Data
 public class UserResponseDto {
+    private Long id;
+    private UserRole role;
+    private String username;
 }
