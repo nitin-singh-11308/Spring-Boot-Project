7556d2cbf5c5f429cb88801f583ce806585b2810
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-10 19:10:52 +0530

    Add Bcrypt Password Encoder and DAO Authentication Provider

--- src/main/java/com/example/MSCafe/config/SecurityConfig.java
+++ src/main/java/com/example/MSCafe/config/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.example.MSCafe.config;
 
+import com.example.MSCafe.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,6 +23,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
@@ -20,14 +34,29 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/").permitAll()
+                                .requestMatchers("/", "/test").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/dishes", "/api/dishes/**").permitAll()
                                 .requestMatchers(HttpMethod.DELETE, "/api/dishes", "/api/dishes/**").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
                                 .requestMatchers("/api", "/api/**").authenticated()
                                 .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+                );
+//               .httpBasic(withDefaults());
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
