1edd59860b193b2df439db356a39e9c85d49d5dd
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-10 19:29:26 +0530

    add Email Request Dto

--- src/main/java/com/example/MSCafe/dto/request/EmailRequestDto.java
+++ src/main/java/com/example/MSCafe/dto/request/EmailRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.MSCafe.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmailRequestDto {
+    @Email
+    @NotBlank
+    private String email;
 }
