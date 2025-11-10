4db4ea40b23fe86fc55f4bd804800614fe191f10
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-10 19:12:28 +0530

    add Email Otp Verification Request Dto

--- src/main/java/com/example/MSCafe/dto/request/EmailOtpVerificationRequestDto.java
+++ src/main/java/com/example/MSCafe/dto/request/EmailOtpVerificationRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.MSCafe.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmailOtpVerificationRequestDto {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String otp;
 }
