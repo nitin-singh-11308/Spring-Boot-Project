066e1e847a50db7ba4ea28962b75ddc53989aa78
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-10 19:06:46 +0530

    Create Email Service Interface And add methods in it

--- src/main/java/com/example/MSCafe/Service/EmailService.java
+++ src/main/java/com/example/MSCafe/Service/EmailService.java
@@ -1,4 +1,11 @@
 package com.example.MSCafe.Service;
 
+import com.example.MSCafe.dto.request.EmailOtpVerificationRequestDto;
+import com.example.MSCafe.dto.request.EmailRequestDto;
+import com.example.MSCafe.dto.response.GenericResponseDto;
+import com.example.MSCafe.enums.OtpPurpose;
+
 public interface EmailService {
+    GenericResponseDto sendOtp(EmailRequestDto emailRequestDto, OtpPurpose otpPurpose);
+    GenericResponseDto verifyOtp(EmailOtpVerificationRequestDto emailOtpVerificationRequestDto, OtpPurpose otpPurpose);
 }
