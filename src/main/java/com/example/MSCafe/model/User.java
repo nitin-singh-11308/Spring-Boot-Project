447aabd7a2a698a7fb61a152ab7b3a55bfe211ea
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:43:53 +0530

    Add attributes and annotation in User entity

--- src/main/java/com/example/MSCafe/model/User.java
+++ src/main/java/com/example/MSCafe/model/User.java
@@ -2,33 +2,67 @@ package com.example.MSCafe.model;
 
 import com.example.MSCafe.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean isEmailVerified = false;
+
+    @Column(nullable = false)
+    private String password;
+
+    // set active = true after successful password setup
+    @Column(nullable = false)
+    private boolean active = false;
+
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    @Column(unique = true)
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = true)
     private String username;
-    private String password;
+
+    @Column(unique = true, nullable = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private boolean isRegistrationComplete = false;
+
+    @CreationTimestamp
+    private LocalDateTime createsAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" +role.name()));
+        return List.of(() -> "ROLE_" + (role != null ? role.name() : UserRole.GUEST.name()));
     }
 
     @Override
