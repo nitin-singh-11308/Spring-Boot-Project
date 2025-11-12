82db962008749d80483442e679344f5a02cab409
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:59:32 +0530

    Create conreoller for Api Endpoint

--- src/main/java/com/example/MSCafe/Controller/AuthController.java
+++ src/main/java/com/example/MSCafe/Controller/AuthController.java
@@ -1,4 +1,33 @@
 package com.example.MSCafe.Controller;
 
+import com.example.MSCafe.Service.EmailService;
+import com.example.MSCafe.dto.request.EmailOtpVerificationRequestDto;
+import com.example.MSCafe.dto.request.EmailRequestDto;
+import com.example.MSCafe.dto.response.GenericResponseDto;
+import com.example.MSCafe.enums.OtpPurpose;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/signup/email/sent-otp")
+    public ResponseEntity<GenericResponseDto>sendOtpToSignup (@Valid @RequestBody EmailRequestDto emailRequestDto){
+        return new ResponseEntity<>(emailService.sendOtp(emailRequestDto, OtpPurpose.SIGNUP), HttpStatusCode.valueOf(200));
+    }
+
+    @PostMapping("/signup/email/verify-otp")
+    public ResponseEntity<GenericResponseDto>sendOtpToVerification (@Valid @RequestBody EmailOtpVerificationRequestDto emailOtpVerificationRequestDto){
+        return new ResponseEntity<>(emailService.verifyOtp(emailOtpVerificationRequestDto, OtpPurpose.SIGNUP), HttpStatus.valueOf(201));
+    }
 }
