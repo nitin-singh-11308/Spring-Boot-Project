cf85de87a1b95fa26ab5b2bd8454b99b18ee2251
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:52:43 +0530

    add Otp Store(Storation to Deletion life cycle in it)

--- src/main/java/com/example/MSCafe/store/OtpStore.java
+++ src/main/java/com/example/MSCafe/store/OtpStore.java
@@ -1,4 +1,16 @@
 package com.example.MSCafe.store;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class OtpStore {
+    private static final ConcurrentHashMap<String, String> otpMap = new ConcurrentHashMap<>();
+    public static void storeOtp(String email, String otp) {
+        otpMap.put(email,otp);
+    }
+    public static String getOtp (String email) {
+        return otpMap.get(email);
+    }
+    public static void clearOtp(String email)  {
+        otpMap.remove(email);
+    }
 }
