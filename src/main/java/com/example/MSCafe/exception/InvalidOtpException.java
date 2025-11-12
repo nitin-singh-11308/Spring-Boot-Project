1451888936d94315a923623d0213182882cd3189
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:57:48 +0530

    Create Invalid OTP Exception

--- src/main/java/com/example/MSCafe/exception/InvalidOtpException.java
+++ src/main/java/com/example/MSCafe/exception/InvalidOtpException.java
@@ -1,7 +1,10 @@
 package com.example.MSCafe.exception;
 
-public class InvaildOtpException extends RuntimeException {
-  public InvaildOtpException(String message) {
-    super(message);
-  }
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException() {
+        super("OTP verification failed ");
+    }
+    public InvalidOtpException(String m) {
+        super(m);
+    }
 }
