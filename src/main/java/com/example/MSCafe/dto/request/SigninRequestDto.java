37224e27915bd88f12517b6d51f3e8c3cb5d18dc
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:55:18 +0530

    Create Signin and 'Add SignIn Attribues'

--- src/main/java/com/example/MSCafe/dto/request/SigninRequestDto.java
+++ src/main/java/com/example/MSCafe/dto/request/SigninRequestDto.java
@@ -1,4 +1,17 @@
 package com.example.MSCafe.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SigninRequestDto {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
