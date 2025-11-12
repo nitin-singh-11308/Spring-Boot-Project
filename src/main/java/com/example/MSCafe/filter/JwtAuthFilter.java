e88213b2aabf8be31f62d64015ed528ccda1b672
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:45:44 +0530

    Add JWT Filter in Servelet Filter Api

--- src/main/java/com/example/MSCafe/filter/JwtAuthFilter.java
+++ src/main/java/com/example/MSCafe/filter/JwtAuthFilter.java
@@ -29,8 +29,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         String authHeader = request.getHeader("Authorization");
 
-
-
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
           String token = authHeader.substring(7);
 
