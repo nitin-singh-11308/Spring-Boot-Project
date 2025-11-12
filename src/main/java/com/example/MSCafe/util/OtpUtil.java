5f2f40daa88a7dd7c09f65273f475a7e6715a003
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-12 16:50:46 +0530

    Add OTP util (Define Otp length and creation in it)

--- src/main/java/com/example/MSCafe/util/OtpUtil.java
+++ src/main/java/com/example/MSCafe/util/OtpUtil.java
@@ -1,4 +1,16 @@
 package com.example.MSCafe.util;
 
+import java.security.SecureRandom;
+
 public class OtpUtil {
+    private static final SecureRandom random = new SecureRandom();
+    private static final int OTP_LENGTH = 6;
+
+    public static String generateOtp() {
+        StringBuilder sb = new StringBuilder(OTP_LENGTH);
+        for (int i = 0; i < OTP_LENGTH; i++) {
+            sb.append(random.nextInt(10)); // 0-9 digit
+        }
+        return sb.toString();
+    }
 }
