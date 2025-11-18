be850d771893ef2095995cfaabe34be0e1d0f53d
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-18 09:26:41 +0530

    Add Email and Password Request Dto

--- src/main/java/com/example/MSCafe/dto/request/EmailAndPasswordRequestDto.java
+++ src/main/java/com/example/MSCafe/dto/request/EmailAndPasswordRequestDto.java
@@ -1,4 +1,23 @@
 package com.example.MSCafe.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmailAndPasswordRequestDto {
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "Password must be 8–15 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character"
+    )
+    private String password;
 }
