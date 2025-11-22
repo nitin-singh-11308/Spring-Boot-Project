aa9669ada35d2a3b08ec9a293b4b53edb89e1d68
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-22 17:30:41 +0530

    Create Auth service 'Interface' and create abstract methods in it

--- src/main/java/com/example/MSCafe/Service/AuthService.java
+++ src/main/java/com/example/MSCafe/Service/AuthService.java
@@ -1,4 +1,12 @@
 package com.example.MSCafe.Service;
 
-public class AuthService {
+import com.example.MSCafe.dto.request.EmailAndPasswordRequestDto;
+import com.example.MSCafe.dto.request.SigninRequestDto;
+import com.example.MSCafe.dto.response.GenericResponseDto;
+
+public interface AuthService {
+    GenericResponseDto signup(String authHeader, EmailAndPasswordRequestDto emailAndPasswordRequestDto);
+    GenericResponseDto signin(SigninRequestDto signinRequestDto);
+    GenericResponseDto resetPassword(String authHeader, EmailAndPasswordRequestDto emailAndPasswordRequestDto);
 }
+
