b0434131ffc564b7b2f639059a593410a5fca591
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-22 17:39:15 +0530

    Refactor code of send OTP with HTML syntex and add implement signin alert method

--- src/main/java/com/example/MSCafe/Service/EmailServiceImpl.java
+++ src/main/java/com/example/MSCafe/Service/EmailServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.MSCafe.dto.request.EmailRequestDto;
 import com.example.MSCafe.dto.response.GenericResponseDto;
 import com.example.MSCafe.enums.OtpPurpose;
 import com.example.MSCafe.exception.InvalidOtpException;
+import com.example.MSCafe.exception.UserNotFoundException;
 import com.example.MSCafe.model.User;
 import com.example.MSCafe.store.OtpStore;
 import com.example.MSCafe.util.JwtUtil;
@@ -17,6 +18,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Service
@@ -57,7 +60,23 @@ public class EmailServiceImpl implements EmailService{
 
         String subject = "MS Cafe - " + (otpPurpose == OtpPurpose.SIGNUP ? "Signup OTP" : "Password Reset OTP");
 
-        String purposeMessage = "<h1>Your OTP is" + otp + "</h1>";
+        String purposeMessage = (otpPurpose == OtpPurpose.SIGNUP) ? "Use the OTP below to complete your signin process."
+                : "Use the OTP below to reset your password.";
+
+        String htmlBody = """
+                <html>
+                    <body style= "font-family:Arial, san-serif; line-height: 1.6; color: #333;">
+                        <h2 style="color: #$CAF50;">MSCafe</h2>
+                        <p>Hello</p>
+                        <p>%s</p>
+                            <div style = "padding: 10px; background-color: #f3f3f3; border-radius; 5px; display: inline-block;">
+                                <h3 style="margin:0; color: #333;">%s</h3>
+                            </div>
+                        <p>This OTP is Valid for <b>5 Minutes</b>. Please do not share it with anyone.</p>
+                        <p>Thank you, <br/>MSCafe Team</p>
+                    </body>
+                </html>
+                """.formatted(purposeMessage,otp);
 
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -65,7 +84,7 @@ public class EmailServiceImpl implements EmailService{
 
             helper.setTo(email);
             helper.setSubject(subject);
-            helper.setText(purposeMessage,true);
+            helper.setText(htmlBody,true);
 
             mailSender.send(mimeMessage);
         }
@@ -125,6 +144,74 @@ public class EmailServiceImpl implements EmailService{
         }
 
         throw new InvalidOtpException("Otp Verification Failed");
+    }
+
+    @Override
+    public GenericResponseDto sendSigninAlert(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new UserNotFoundException("User email: " +email+ " doesn't exist"));
+
+        StringBuilder userFullName = new StringBuilder();
+        userFullName.append((user.getFirstName() != null) ? user.getFirstName() : " " );
+        userFullName.append(user.getLastName() != null ? " " + user.getLastName() : " ");
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZoneOffset zoneOffset = OffsetDateTime.now(zoneId).getOffset();
+
+        String currentDate = currentDateTime.format(dateFormatter);
+        String currentTime = currentDateTime.format(timeFormatter);
+        String currentTimeZoneId = String.format("%s (UTC%s)", zoneId.getId(), zoneOffset);
+
+        String time = currentDate + " " + currentTime + " " + currentTimeZoneId;
+
+        String ipAddress = "Unknown";
+        String device = "Unknown";
+
+        String subject = "MSCafe - Signin Alert";
+
+        String htmlBody = """
+                   <html>
+                        <body style = "font-family: Arial, sans-sarif; line-height: 1.6; color: #333; ">
+                            <h2 style ="color: #4CAF50;">MSCafe</h2>
+                            <p><strong>Sign-in Details:</strong></p>
+                
+                            <ul>
+                                <li>Time: %s</li>
+                                <li>IP Address: %s</li>
+                                <li>Device: %s</li>
+                            </ul>
+                
+                           <p>If this was you, no further action is needed. If you did not sign-in, please reset your password immediately.</p>
+                           <p>Thank you, <br/>MSCafe Team</p>
+                       </body>
+                  </html>
+                """.formatted(userFullName,time, ipAddress,device);
+
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true,"UTF-8");
+
+            helper.setTo(email);
+            helper.setSubject(subject);
+            helper.setText(htmlBody, true); // true enable HTML
+
+            mailSender.send(mimeMessage);
+        }catch (Exception e) {
+            return GenericResponseDto.builder()
+                    .success(false)
+                    .message("Failed to sent OTP email. Please try again later.")
+                    .build();
+
+        }
+        return GenericResponseDto.builder()
+                .success(true)
+                .message("OTP sent to "+email)
+                .detail(Map.of("purpose", "Sign-in Alert"))
+                .build();
+
     }
 
 }
