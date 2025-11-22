b1dc70e827d0a29b64004fd4847d48cc1576a4d3
nitin-singh-11308 <nitinsingh9963@gmail.com>
2025-11-22 17:41:00 +0530

    Add signup and signin API controller endpoint

--- src/main/java/com/example/MSCafe/Controller/AuthController.java
+++ src/main/java/com/example/MSCafe/Controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.example.MSCafe.Controller;
 
+import com.example.MSCafe.Service.AuthService;
 import com.example.MSCafe.Service.EmailService;
+import com.example.MSCafe.dto.request.EmailAndPasswordRequestDto;
 import com.example.MSCafe.dto.request.EmailOtpVerificationRequestDto;
 import com.example.MSCafe.dto.request.EmailRequestDto;
+import com.example.MSCafe.dto.request.SigninRequestDto;
 import com.example.MSCafe.dto.response.GenericResponseDto;
 import com.example.MSCafe.enums.OtpPurpose;
 import jakarta.validation.Valid;
@@ -10,16 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private AuthService authService;
 
     @PostMapping("/signup/email/sent-otp")
     public ResponseEntity<GenericResponseDto>sendOtpToSignup (@Valid @RequestBody EmailRequestDto emailRequestDto){
@@ -29,5 +32,15 @@ public class AuthController {
     @PostMapping("/signup/email/verify-otp")
     public ResponseEntity<GenericResponseDto>sendOtpToVerification (@Valid @RequestBody EmailOtpVerificationRequestDto emailOtpVerificationRequestDto){
         return new ResponseEntity<>(emailService.verifyOtp(emailOtpVerificationRequestDto, OtpPurpose.SIGNUP), HttpStatus.valueOf(201));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<GenericResponseDto> signup(@RequestHeader("Authorization") String authHeader, @Valid @RequestBody EmailAndPasswordRequestDto emailAndPasswordRequestDto) {
+        return new ResponseEntity<>(authService.signup(authHeader, emailAndPasswordRequestDto), HttpStatus.OK);
+    }
+
+    @PostMapping("/signin")
+    public  ResponseEntity<GenericResponseDto> signin(@Valid @RequestBody SigninRequestDto signinRequestDto) {
+        return new ResponseEntity<>(authService.signin(signinRequestDto), HttpStatusCode.valueOf(200));
     }
 }
